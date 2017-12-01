4a219c95cd33a535f53612248053fc729b90db51
Paul Hermany <PHermany@rightpoint.com>
2017-11-30 22:57:55 -0600

    added day01 basics

---
+++ Hermany.AoC/Day01/Day01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermany.AoC.Day01
+{
+    public class Day01
+    {
+        public string Part1(string[] input)
+        {
+            return string.Empty;
+        }
+
+        public string Part2(string[] input)
+        {
+            return string.Empty;
+        }
+    }
+}
--- Hermany.AoC/Program.cs
+++ Hermany.AoC/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace Hermany.AoC
     {
         public static void Main(string[] args)
         {
+            var pathToExe = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+            
+            var input = File.ReadAllLines(Path.Combine(pathToExe, "Day01/input.txt"));
+            var day01 = new Day01.Day01();
+
+            var part1 = day01.Part1(input);
+            File.WriteAllText(Path.Combine(pathToExe, "Day01/output-part1.txt"), part1);
+            Console.WriteLine(part1);
+
+            var part2 = day01.Part2(input);
+            File.WriteAllText(Path.Combine(pathToExe, "Day01/output-part2.txt"), part2);
+            Console.WriteLine(part2);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
