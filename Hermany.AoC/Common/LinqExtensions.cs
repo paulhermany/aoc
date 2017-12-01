7b368a771327e11e2335181215614b608f4deeee
Paul Hermany <PHermany@rightpoint.com>
2017-11-30 23:35:17 -0600

    completed day01

---
+++ Hermany.AoC/Common/LinqExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hermany.AoC.Common
+{
+    public static class LinqExtensions
+    {
+        public static int CaptchaSum(this IEnumerable<int> values, int compareIndexOffset = 1)
+        {
+            var arr = values.ToArray();
+            return arr.Where((t, i) => t == arr[(i + compareIndexOffset) % arr.Length]).Sum();
+        }
+    }
+}
--- Hermany.AoC/Day01/Day01.cs
+++ Hermany.AoC/Day01/Day01.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Hermany.AoC.Common;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hermany.AoC.Day01
 {
     public class Day01
     {
-        public string Part1(string[] input)
-        {
-            return string.Empty;
-        }
+        public string Part1(string[] input) => 
+            input[0].Select(_ => int.Parse(_.ToString())).CaptchaSum().ToString();
 
-        public string Part2(string[] input)
-        {
-            return string.Empty;
-        }
+        public string Part2(string[] input) =>
+            input[0].Select(_ => int.Parse(_.ToString())).CaptchaSum(input[0].Length / 2).ToString();
     }
 }
