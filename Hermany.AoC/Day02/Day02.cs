068f8e4e2a107d02b1e251368dc18aebd234708b
Paul Hermany <PHermany@rightpoint.com>
2017-12-01 23:16:05 -0600

    completed day02

--- Hermany.AoC/Day02/Day02.cs
+++ Hermany.AoC/Day02/Day02.cs
@@ -1,4 +1,6 @@
-﻿using Hermany.AoC.Common;
+﻿using System;
+using System.CodeDom;
+using Hermany.AoC.Common;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
@@ -9,12 +11,35 @@ namespace Hermany.AoC.Day02
     {
         public string Part1(string[] input)
         {
-            return string.Empty;
+            var sum = input.Select(line => line.Split('\t').Select(_ => int.Parse(_.Trim())).ToArray()).Select(arr => arr.Max() - arr.Min()).Sum();
+            return sum.ToString();
         }
 
         public string Part2(string[] input)
         {
-            return string.Empty;
+            var sum = 0;
+            foreach (var line in input)
+            {
+                var arr = line.Split('\t').Select(_ => int.Parse(_.Trim())).ToArray();
+
+                for (var i = 0; i < arr.Length; i++)
+                {
+                    for (var j = i + 1; j < arr.Length; j++)
+                    {
+                        if (arr[i] > arr[j])
+                        {
+                            if (arr[i] % arr[j] == 0)
+                                sum += arr[i] / arr[j];
+                        }
+                        else
+                        {
+                            if (arr[j] % arr[i] == 0)
+                                sum += arr[j] / arr[i];
+                        }
+                    }
+                }
+            }
+            return sum.ToString();
         }
     }
 }
