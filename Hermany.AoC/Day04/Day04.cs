c4b5805d0a5b00a6a510e6e9137a2c01954fc9cf
Paul Hermany <PHermany@rightpoint.com>
2017-12-03 23:33:28 -0600

    completed day04

--- Hermany.AoC/Day04/Day04.cs
+++ Hermany.AoC/Day04/Day04.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Hermany.AoC.Common;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Hermany.AoC.Day04
@@ -12,12 +13,18 @@ namespace Hermany.AoC.Day04
     {
         public string Part1(string[] input)
         {
-            return string.Empty;
-        }
+            var sum = input.Select(passphrase => passphrase.Split(' '))
+                .Count(words => words.Distinct().Count() == words.Length);
 
+            return sum.ToString();
+        }
+        
         public string Part2(string[] input)
         {
-            return string.Empty;
+            var sum = input.Select(passphrase => passphrase.Split(' ').Select(word => string.Concat(word.OrderBy(c => c))))
+                .Count(words => words.Distinct().Count() == words.Count());
+            
+            return sum.ToString();
         }
     }
 }
