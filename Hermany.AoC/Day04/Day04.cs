034be14f93b10e014680ae1e685b82671f9e9c36
Paul Hermany <PHermany@rightpoint.com>
2017-12-03 23:37:36 -0600

    refactored using expression body

--- Hermany.AoC/Day04/Day04.cs
+++ Hermany.AoC/Day04/Day04.cs
@@ -11,20 +11,12 @@ namespace Hermany.AoC.Day04
 {
     public class Day04
     {
-        public string Part1(string[] input)
-        {
-            var sum = input.Select(passphrase => passphrase.Split(' '))
-                .Count(words => words.Distinct().Count() == words.Length);
-
-            return sum.ToString();
-        }
+        public string Part1(string[] input) =>
+            input.Select(passphrase => passphrase.Split(' '))
+                .Count(words => words.Distinct().Count() == words.Length).ToString();
         
-        public string Part2(string[] input)
-        {
-            var sum = input.Select(passphrase => passphrase.Split(' ').Select(word => string.Concat(word.OrderBy(c => c))))
-                .Count(words => words.Distinct().Count() == words.Count());
-            
-            return sum.ToString();
-        }
+        public string Part2(string[] input) =>
+            input.Select(passphrase => passphrase.Split(' ').Select(word => string.Concat(word.OrderBy(c => c))))
+                .Count(words => words.Distinct().Count() == words.Count()).ToString();
     }
 }
