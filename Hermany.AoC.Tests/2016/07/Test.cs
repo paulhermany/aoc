cd7b40a5963bc46e303ec0528dc7fb0756d2afca
Paul Hermany <PHermany@rightpoint.com>
2017-12-04 22:46:52 -0600

    added 2016 07 part 1

---
+++ Hermany.AoC.Tests/2016/07/Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hermany.AoC.Tests._2016._07
+{
+    [TestClass]
+    public class Test
+    {
+        public ISolution CreateSolution() =>
+            new AoC._2016._07.Solution();
+
+        [TestMethod]
+        public void Part1SampleInput()
+        {
+            var solution = CreateSolution();
+            Assert.AreEqual(solution.Part1("abba[mnop]qrst"), "1");
+            Assert.AreEqual(solution.Part1("abcd[bddb]xyyx"), "0");
+            Assert.AreEqual(solution.Part1("aaaa[qwer]tyui"), "0");
+            Assert.AreEqual(solution.Part1("ioxxoj[asdfgh]zxcvbn"), "1");
+        }
+
+        [TestMethod]
+        public void Part1PuzzleInput()
+        {
+            var solution = CreateSolution();
+            Assert.AreEqual(solution.Part1(Program.GetPuzzleInput(solution)), "118");
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void Part2SampleInput()
+        {
+            var solution = CreateSolution();
+            Assert.AreEqual(solution.Part1("aba[bab]xyz"), "1");
+            Assert.AreEqual(solution.Part1("xyx[xyx]xyx"), "0");
+            Assert.AreEqual(solution.Part1("aaa[kek]eke"), "1");
+            Assert.AreEqual(solution.Part1("zazbz[bzb]cdb"), "1");
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void Part2PuzzleInput()
+        {
+            var solution = CreateSolution();
+            Assert.AreEqual(solution.Part2(Program.GetPuzzleInput(solution)), "260");
+        }
+    }
+}
--- Hermany.AoC/2016/07/Solution.cs
+++ Hermany.AoC/2016/07/Solution.cs
@@ -1,12 +1,20 @@
 ﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using Hermany.AoC.Common;
 
 namespace Hermany.AoC._2016._07
 {
     public class Solution : ISolution
     {
+        public const string AbbaPattern = @"(\w)(?!\1)(\w)\2\1";
+        public const string HypernetAbbaPattern = @"\[\w*(\w)(?!\1)(\w)\2\1\w*\]";
+
         public string Part1(params string[] input) =>
-            string.Empty;
+            input.Count(_ =>
+                Regex.IsMatch(_, AbbaPattern)  &&
+                !Regex.IsMatch(_, HypernetAbbaPattern)
+            ).ToString();
 
         public string Part2(params string[] input) =>
             string.Empty;
