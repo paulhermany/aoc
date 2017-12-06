314020e8b3576fd33da19c2939c1e6de5af3ab8e
Paul Hermany <PHermany@rightpoint.com>
2017-12-06 11:34:40 -0600

    refactored tests for 2016 04 to flip expected/actual

--- Hermany.AoC.Tests/2016/04/Test.cs
+++ Hermany.AoC.Tests/2016/04/Test.cs
@@ -14,21 +14,21 @@ namespace Hermany.AoC.Tests._2016._04
         public void Part1SampleInput()
         {
             var solution = CreateSolution();
-            Assert.AreEqual(solution.Part1("aaaaa-bbb-z-y-x-123[abxyz]", "a-b-c-d-e-f-g-h-987[abcde]", "not-a-real-room-404[oarel]", "totally-real-room-200[decoy]"), "1514");
+            Assert.AreEqual("1514", solution.Part1("aaaaa -bbb-z-y-x-123[abxyz]", "a-b-c-d-e-f-g-h-987[abcde]", "not-a-real-room-404[oarel]", "totally-real-room-200[decoy]"));
         }
 
         [TestMethod]
         public void Part1PuzzleInput()
         {
             var solution = CreateSolution();
-            Assert.AreEqual(solution.Part1(Program.GetPuzzleInput(solution)), "361724");
+            Assert.AreEqual("361724", solution.Part1(Program.GetPuzzleInput(solution)));
         }
 
         [TestMethod]
         public void Part2PuzzleInput()
         {
             var solution = CreateSolution();
-            Assert.AreEqual(solution.Part2(Program.GetPuzzleInput(solution)), "482");
+            Assert.AreEqual("482", solution.Part2(Program.GetPuzzleInput(solution)));
         }
     }
 }
