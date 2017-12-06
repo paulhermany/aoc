b7a062d980293fd53cefb1ef8ca1e93d42d1f0f0
Paul Hermany <PHermany@rightpoint.com>
2017-12-06 11:31:41 -0600

    refactored tests for 2016 02 to flip expected/actual

--- Hermany.AoC.Tests/2016/02/Test.cs
+++ Hermany.AoC.Tests/2016/02/Test.cs
@@ -14,28 +14,28 @@ namespace Hermany.AoC.Tests._2016._02
         public void Part1SampleInput()
         {
             var solution = CreateSolution();
-            Assert.AreEqual(solution.Part1("ULL","RRDDD","LURDL","UUUUD"), "1985");
+            Assert.AreEqual("1985", solution.Part1("ULL","RRDDD","LURDL","UUUUD"));
         }
 
         [TestMethod]
         public void Part1PuzzleInput()
         {
             var solution = CreateSolution();
-            Assert.AreEqual(solution.Part1(Program.GetPuzzleInput(solution)), "53255");
+            Assert.AreEqual("53255", solution.Part1(Program.GetPuzzleInput(solution)));
         }
 
         [TestMethod]
         public void Part2SampleInput()
         {
             var solution = CreateSolution();
-            Assert.AreEqual(solution.Part2("ULL", "RRDDD", "LURDL", "UUUUD"), "5DB3");
+            Assert.AreEqual("5DB3", solution.Part2("ULL", "RRDDD", "LURDL", "UUUUD"));
         }
 
         [TestMethod]
         public void Part2PuzzleInput()
         {
             var solution = CreateSolution();
-            Assert.AreEqual(solution.Part2(Program.GetPuzzleInput(solution)), "7423A");
+            Assert.AreEqual("7423A", solution.Part2(Program.GetPuzzleInput(solution)));
         }
     }
 }
