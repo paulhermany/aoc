0c104f9f3a874ee19dd0e51a825b6ff5da85dc31
Paul Hermany <PHermany@rightpoint.com>
2017-12-13 09:24:40 -0600

    completed day13

---
+++ Hermany.AoC.Tests/2017/13/Test.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hermany.AoC.Tests._2017._13
+{
+    [TestClass]
+    public class Test
+    {
+        public ISolution CreateSolution() =>
+            new AoC._2017._13.Solution();
+
+        [TestMethod]
+        public void Part1SampleInput()
+        {
+            var solution = CreateSolution();
+            Assert.AreEqual("24", solution.Part1(
+                "0: 3",
+                "1: 2",
+                "4: 4",
+                "6: 4"));
+        }
+
+        [TestMethod]
+        public void Part1PuzzleInput()
+        {
+            var solution = CreateSolution();
+            var input = Program.GetPuzzleInput(solution);
+            Assert.AreEqual("1840", solution.Part1(input));
+        }
+
+        [TestMethod]
+        public void Part2SampleInput()
+        {
+            var solution = CreateSolution();
+            Assert.AreEqual("10", solution.Part2(
+                "0: 3",
+                "1: 2",
+                "4: 4",
+                "6: 4"));
+        }
+
+        [TestMethod]
+        public void Part2PuzzleInput()
+        {
+            var solution = CreateSolution();
+            var input = Program.GetPuzzleInput(solution);
+            Assert.AreEqual("3850260", solution.Part2(input));
+        }
+    }
+}
---
+++ Hermany.AoC/2017/13/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Hermany.AoC._2017._13
+{
+    public class Solution : ISolution
+    {
+        public static int ReflectiveMod(int value, int modulus)
+        {
+            // ReflectiveMod(6, 4) = 0
+            // value: 0 1 2 3 4 5 6 7 8 9 10
+            // r-mod: 0 1 2 3 2 1 0 1 2 3 4 
+
+            var p = (modulus - 1) * 2;
+            var v = value % p;
+            return v >= p / 2 ? p - v : v;
+            
+        }
+        public string Part1(params string[] input)
+        {
+            var ranges = input.Select(line =>
+            {
+                var tokens = line.Split(':').Select(_ => _.Trim()).ToArray();
+                return new { Index = int.Parse(tokens[0]), Range = int.Parse(tokens[1]) };
+            }).ToArray();
+
+            return ranges.Where(_ => ReflectiveMod(_.Index, _.Range) == 0).Sum(_ => _.Index * _.Range).ToString();
+        }
+        
+        public string Part2(params string[] input)
+        {
+            var ranges = input.Select(line =>
+            {
+                var tokens = line.Split(':').Select(_ => _.Trim()).ToArray();
+                return new { Index = int.Parse(tokens[0]), Range = int.Parse(tokens[1]) };
+            }).ToArray();
+
+            var i = 0;
+
+            while (ranges.Any(_ => ReflectiveMod(_.Index + i, _.Range) == 0)) i++;
+
+            return i.ToString();
+        }
+    }
+}
