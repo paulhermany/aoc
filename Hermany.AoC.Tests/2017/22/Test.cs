ff718baf6e04926eadf03ae9a2f37bb1cd3b3e11
Paul Hermany <PHermany@rightpoint.com>
2017-12-18 19:08:30 -0600

    added skeleton code for remainder of 2017

---
+++ Hermany.AoC.Tests/2017/22/Test.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hermany.AoC.Tests._2017._22
+{
+    [TestClass]
+    public class Test
+    {
+        public ISolution CreateSolution() =>
+            new AoC._2017._22.Solution();
+
+        [TestMethod]
+        public void Part1SampleInput()
+        {
+            var solution = CreateSolution();
+            var input = Program.GetPuzzleInput(solution, "input.sample.txt");
+            Assert.AreEqual("", solution.Part1(input));
+        }
+
+        [TestMethod]
+        public void Part1PuzzleInput()
+        {
+            var solution = CreateSolution();
+            var input = Program.GetPuzzleInput(solution);
+            Assert.AreEqual("", solution.Part1(input));
+        }
+
+        [TestMethod]
+        public void Part2SampleInput()
+        {
+            var solution = CreateSolution();
+            var input = Program.GetPuzzleInput(solution, "input.sample.txt");
+            Assert.AreEqual("", solution.Part2(input));
+        }
+
+        [TestMethod]
+        public void Part2PuzzleInput()
+        {
+            var solution = CreateSolution();
+            var input = Program.GetPuzzleInput(solution);
+            Assert.AreEqual("", solution.Part2(input));
+        }
+    }
+}
---
+++ Hermany.AoC/2017/20/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Hermany.AoC._2017._20
+{
+    public class Solution : ISolution
+    {
+        public string Part1(params string[] input)
+        {
+            return string.Empty;
+        }
+        
+        public string Part2(params string[] input)
+        {
+            return string.Empty;
+        }
+    }
+}
