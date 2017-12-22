72fbcebaf0328cc6f9f7912b9083d3cfa2cb8919
Paul Hermany <PHermany@rightpoint.com>
2017-12-21 18:12:45 -0600

    completed day21

--- Hermany.AoC.Tests/2017/21/Test.cs
+++ Hermany.AoC.Tests/2017/21/Test.cs
@@ -12,32 +12,36 @@ namespace Hermany.AoC.Tests._2017._21
         public void Part1SampleInput()
         {
             var solution = CreateSolution();
+            ((AoC._2017._21.Solution) solution).NumberOfIterations = 2;
             var input = Program.GetPuzzleInput(solution, "input.sample.txt");
-            Assert.AreEqual("", solution.Part1(input));
+            Assert.AreEqual("12", solution.Part1(input));
         }
 
         [TestMethod]
         public void Part1PuzzleInput()
         {
             var solution = CreateSolution();
+            ((AoC._2017._21.Solution)solution).NumberOfIterations = 5;
             var input = Program.GetPuzzleInput(solution);
-            Assert.AreEqual("", solution.Part1(input));
+            Assert.AreEqual("144", solution.Part1(input));
         }
 
         [TestMethod]
         public void Part2SampleInput()
         {
             var solution = CreateSolution();
+            ((AoC._2017._21.Solution)solution).NumberOfIterations = 2;
             var input = Program.GetPuzzleInput(solution, "input.sample.txt");
-            Assert.AreEqual("", solution.Part2(input));
+            Assert.AreEqual("12", solution.Part2(input));
         }
 
         [TestMethod]
         public void Part2PuzzleInput()
         {
             var solution = CreateSolution();
+            ((AoC._2017._21.Solution)solution).NumberOfIterations = 18;
             var input = Program.GetPuzzleInput(solution);
-            Assert.AreEqual("", solution.Part2(input));
+            Assert.AreEqual("2169301", solution.Part2(input));
         }
     }
 }
