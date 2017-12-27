ba1d606add33efed9da6f71e9d45c7e01d3bbc7d
Paul Hermany <PHermany@rightpoint.com>
2017-12-27 12:15:13 -0600

    completed day25

--- Hermany.AoC/2017/25/Solution.cs
+++ Hermany.AoC/2017/25/Solution.cs
@@ -9,7 +9,102 @@ namespace Hermany.AoC._2017._25
     {
         public string Part1(params string[] input)
         {
-            return string.Empty;
+            var state = 'A';
+
+            var tape = new Dictionary<int, bool>();
+            var cursor = 0;
+
+            for (var i = 0; i < 12425180; i++)
+            {
+                if (!tape.ContainsKey(cursor)) tape.Add(cursor, false);
+                
+                switch (state)
+                {
+                    case 'A':
+                        if (!tape[cursor])
+                        {
+                            tape[cursor] = true;
+                            cursor++;
+                            state = 'B';
+                        }
+                        else
+                        {
+                            tape[cursor] = false;
+                            cursor++;
+                            state = 'F';
+                        }
+                        break;
+                    case 'B':
+                        if (!tape[cursor])
+                        {
+                            cursor--;
+                            state = 'B';
+                        }
+                        else
+                        {
+                            cursor--;
+                            state = 'C';
+                        }
+                        break;
+                    case 'C':
+                        if (!tape[cursor])
+                        {
+                            tape[cursor] = true;
+                            cursor--;
+                            state = 'D';
+                        }
+                        else
+                        {
+                            tape[cursor] = false;
+                            cursor++;
+                            state = 'C';
+                        }
+                        break;
+                    case 'D':
+                        if (!tape[cursor])
+                        {
+                            tape[cursor] = true;
+                            cursor--;
+                            state = 'E';
+                        }
+                        else
+                        {
+                            cursor++;
+                            state = 'A';
+                        }
+                        break;
+                    case 'E':
+                        if (!tape[cursor])
+                        {
+                            tape[cursor] = true;
+                            cursor--;
+                            state = 'F';
+                        }
+                        else
+                        {
+                            tape[cursor] = false;
+                            cursor--;
+                            state = 'D';
+                        }
+                        break;
+                    case 'F':
+                        if (!tape[cursor])
+                        {
+                            tape[cursor] = true;
+                            cursor++;
+                            state = 'A';
+                        }
+                        else
+                        {
+                            tape[cursor] = false;
+                            cursor--;
+                            state = 'E';
+                        }
+                        break;
+                }
+            }
+            
+            return tape.Count(_ => _.Value).ToString();
         }
         
         public string Part2(params string[] input)
