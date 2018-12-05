5910d76156698bc604a6fb89108af0925cfab7ea
Paul Hermany <PHermany@rightpoint.com>
2018-12-05 07:55:42 -0600

    completed day 05

--- Hermany.AoC/_2018/_05/Solution.cs
+++ Hermany.AoC/_2018/_05/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Hermany.AoC.Common;
 
 namespace Hermany.AoC._2018._05
@@ -7,12 +8,54 @@ namespace Hermany.AoC._2018._05
     {
         public string Part1(params string[] input)
         {
-            throw new NotImplementedException();
+            var units = input[0];
+            var i = 0;
+
+            while (i + 1 < units.Length)
+            {
+                while (i > 0 && i+1 < units.Length && units[i] != units[i + 1] &&
+                       (units[i].ToString() == units[i + 1].ToString().ToLower() ||
+                        units[i].ToString().ToLower() == units[i + 1].ToString()))
+                {
+                    units = units.Remove(i, 2);
+                    i--;
+                }
+
+                i++;
+            }
+
+            return units.Length.ToString();
         }
 
         public string Part2(params string[] input)
         {
-            throw new NotImplementedException();
+            var units = string.Empty;
+            var i = 0;
+
+            var minLength = input[0].Length;
+
+            foreach (var c in "abcdefghijklmnopqrstuvwxyz")
+            {
+                units = input[0].Replace(c.ToString(), string.Empty).Replace(c.ToString().ToUpper(), string.Empty);
+                i = 0;
+
+                while (i + 1 < units.Length)
+                {
+                    while (i > 0 && i + 1 < units.Length && units[i] != units[i + 1] &&
+                           (units[i].ToString() == units[i + 1].ToString().ToLower() ||
+                            units[i].ToString().ToLower() == units[i + 1].ToString()))
+                    {
+                        units = units.Remove(i, 2);
+                        i--;
+                    }
+
+                    i++;
+                }
+
+                if (units.Length < minLength) minLength = units.Length;
+            }
+
+            return minLength.ToString();
         }
         
     }
